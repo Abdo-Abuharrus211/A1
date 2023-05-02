27d03bd2947920f5306a87322d14e8e688448264
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-05-01 18:05:08 -0700

    Reorg directory

--- utils.js
+++
@@ -1,9 +0,0 @@
-// define the include function for absolut file path
-
-global.base_dir = __dirname;
-global.abs_path = function(path) {
-    return base_dir + path;
-}
-global.include = function(file) {
-    return require(abs_path('/' + file));
-}
